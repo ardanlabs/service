e745b9e0fc730e70575f26a25559aa1962ff296b
William Kennedy <bill@ardanlabs.com>
2017-11-20 09:31:27 -0800

    inital code
    Former-commit-id: 2bd554d3a34a9b6691910cac62d7e3691307a7ec

---
+++ cmd/crud/handlers/routes.go
@@ -0,0 +1,20 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/ardanlabs/service/internal/platform/web"
+)
+
+// API returns a handler for a set of routes.
+func API() http.Handler {
+
+	// Create the application.
+	app := web.New()
+
+	// Bind all the user handlers.
+	var u User
+	app.Handle("GET", "/v1/users", u.List)
+
+	return app
+}
