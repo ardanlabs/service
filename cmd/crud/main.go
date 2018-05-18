1730f8922e444969386cdf41cc28fea29673bbc0
William Kennedy <bill@ardanlabs.com>
2018-05-18 11:42:56 -0400

    found link to pprof solution
    Former-commit-id: fd2b98d64096a4d618eab246fb5905f03e0a82d2

--- cmd/crud/main.go
+++ cmd/crud/main.go
@@ -29,7 +29,7 @@ Need to figure out timeouts for http service.
 You might want to reset your DB_HOST env var during test tear down.
 Add pulling git version from build command line.
 Service should start even without a DB running yet.
-symbols in profiles: https://github.com/golang/go/issues/23376
+symbols in profiles: https://github.com/golang/go/issues/23376 / https://github.com/google/pprof/pull/366
 */
 
 func init() {
