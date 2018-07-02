7c358450159e1d526fdf14fab6d5bf50c87f0c62
Jacob Walker <jacob@ardanlabs.com>
2018-07-02 12:24:45 -0500

    Minor text changes

--- internal/platform/flag/doc.go
+++ internal/platform/flag/doc.go
@@ -3,7 +3,7 @@ Package flag is compatible with the GNU extensions to the POSIX recommendations
 for command-line options. See
 http://www.gnu.org/software/libc/manual/html_node/Argument-Syntax.html
 
-There is no hard bindings for this package. This package takes a struct
+There are no hard bindings for this package. This package takes a struct
 value and parses it for flags. It supports three tags to customize the
 flag options.
 
@@ -19,7 +19,7 @@ As an example, this config struct:
 	var cfg struct {
 		Web struct {
 			APIHost     string        `default:"0.0.0.0:3000" flag:"a" flagdesc:"The ip:port for the api endpoint."`
-			BatchSize   int           `default:"1000" flagdesc:"Represets number of items to move."`
+			BatchSize   int           `default:"1000" flagdesc:"Represents number of items to move."`
 			ReadTimeout time.Duration `default:"5s"`
 		}
 		DialTimeout time.Duration `default:"5s"`
@@ -29,7 +29,7 @@ As an example, this config struct:
 Would produce the following flag output:
 
 	-a --web_apihost string  <0.0.0.0:3000> : The ip:port for the api endpoint.
-	--web_batchsize int  <1000> : Represets number of items to move.
+	--web_batchsize int  <1000> : Represents number of items to move.
 	--web_readtimeout Duration  <5s>
 	--dialtimeout Duration  <5s>
 	-h --host string  <mongo:27017/gotraining>
