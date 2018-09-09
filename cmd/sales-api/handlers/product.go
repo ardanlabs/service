ca01c88899d39185c404e0b98d09d74200d76893
Jacob Walker <jacob@ardanlabs.com>
2018-09-09 12:04:35 -0700

    Add product CRUD endpoints and tests.
    Former-commit-id: bd826f895028e43dd9e62e4f17812202569b2f97

---
+++ cmd/sales-api/handlers/product.go
@@ -0,0 +1,116 @@
+package handlers
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/ardanlabs/service/internal/platform/db"
+	"github.com/ardanlabs/service/internal/platform/web"
+	"github.com/ardanlabs/service/internal/product"
+	"github.com/pkg/errors"
+	"go.opencensus.io/trace"
+)
+
+// Product represents the Product API method handler set.
+type Product struct {
+	MasterDB *db.DB
+
+	// ADD OTHER STATE LIKE THE LOGGER AND CONFIG HERE.
+}
+
+// List returns all the existing products in the system.
+func (p *Product) List(ctx context.Context, log *log.Logger, w http.ResponseWriter, r *http.Request, params map[string]string) error {
+	ctx, span := trace.StartSpan(ctx, "handlers.Product.List")
+	defer span.End()
+
+	dbConn := p.MasterDB.Copy()
+	defer dbConn.Close()
+
+	products, err := product.List(ctx, dbConn)
+	if err = check(err); err != nil {
+		return errors.Wrap(err, "")
+	}
+
+	web.Respond(ctx, log, w, products, http.StatusOK)
+	return nil
+}
+
+// Retrieve returns the specified product from the system.
+func (p *Product) Retrieve(ctx context.Context, log *log.Logger, w http.ResponseWriter, r *http.Request, params map[string]string) error {
+	ctx, span := trace.StartSpan(ctx, "handlers.Product.Retrieve")
+	defer span.End()
+
+	dbConn := p.MasterDB.Copy()
+	defer dbConn.Close()
+
+	prod, err := product.Retrieve(ctx, dbConn, params["id"])
+	if err = check(err); err != nil {
+		return errors.Wrapf(err, "ID: %s", params["id"])
+	}
+
+	web.Respond(ctx, log, w, prod, http.StatusOK)
+	return nil
+}
+
+// Create inserts a new product into the system.
+func (p *Product) Create(ctx context.Context, log *log.Logger, w http.ResponseWriter, r *http.Request, params map[string]string) error {
+	ctx, span := trace.StartSpan(ctx, "handlers.Product.Create")
+	defer span.End()
+
+	dbConn := p.MasterDB.Copy()
+	defer dbConn.Close()
+
+	var np product.NewProduct
+	if err := web.Unmarshal(r.Body, &np); err != nil {
+		return errors.Wrap(err, "")
+	}
+
+	nUsr, err := product.Create(ctx, dbConn, &np, time.Now().UTC())
+	if err = check(err); err != nil {
+		return errors.Wrapf(err, "Product: %+v", &np)
+	}
+
+	web.Respond(ctx, log, w, nUsr, http.StatusCreated)
+	return nil
+}
+
+// Update updates the specified product in the system.
+func (p *Product) Update(ctx context.Context, log *log.Logger, w http.ResponseWriter, r *http.Request, params map[string]string) error {
+	ctx, span := trace.StartSpan(ctx, "handlers.Product.Update")
+	defer span.End()
+
+	dbConn := p.MasterDB.Copy()
+	defer dbConn.Close()
+
+	var up product.UpdateProduct
+	if err := web.Unmarshal(r.Body, &up); err != nil {
+		return errors.Wrap(err, "")
+	}
+
+	err := product.Update(ctx, dbConn, params["id"], up, time.Now().UTC())
+	if err = check(err); err != nil {
+		return errors.Wrapf(err, "ID: %s Update: %+v", params["id"], up)
+	}
+
+	web.Respond(ctx, log, w, nil, http.StatusNoContent)
+	return nil
+}
+
+// Delete removed the specified product from the system.
+func (p *Product) Delete(ctx context.Context, log *log.Logger, w http.ResponseWriter, r *http.Request, params map[string]string) error {
+	ctx, span := trace.StartSpan(ctx, "handlers.Product.Delete")
+	defer span.End()
+
+	dbConn := p.MasterDB.Copy()
+	defer dbConn.Close()
+
+	err := product.Delete(ctx, dbConn, params["id"])
+	if err = check(err); err != nil {
+		return errors.Wrapf(err, "Id: %s", params["id"])
+	}
+
+	web.Respond(ctx, log, w, nil, http.StatusNoContent)
+	return nil
+}
--- cmd/sales-api/handlers/routes.go
+++ cmd/sales-api/handlers/routes.go
@@ -13,6 +13,12 @@ import (
 func API(log *log.Logger, masterDB *db.DB) http.Handler {
 	app := web.New(log, mid.RequestLogger, mid.Metrics, mid.ErrorHandler)
 
+	// Register health check endpoint.
+	h := Health{
+		MasterDB: masterDB,
+	}
+	app.Handle("GET", "/v1/health", h.Check)
+
 	u := User{
 		MasterDB: masterDB,
 	}
@@ -22,10 +28,15 @@ func API(log *log.Logger, masterDB *db.DB) http.Handler {
 	app.Handle("PUT", "/v1/users/:id", u.Update)
 	app.Handle("DELETE", "/v1/users/:id", u.Delete)
 
-	h := Health{
+	// Register product and sale endpoints.
+	p := Product{
 		MasterDB: masterDB,
 	}
-	app.Handle("GET", "/v1/health", h.Check)
+	app.Handle("GET", "/v1/products", p.List)
+	app.Handle("POST", "/v1/products", p.Create)
+	app.Handle("GET", "/v1/products/:id", p.Retrieve)
+	app.Handle("PUT", "/v1/products/:id", p.Update)
+	app.Handle("DELETE", "/v1/products/:id", p.Delete)
 
 	return app
 }
