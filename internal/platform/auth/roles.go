21979e14a259e96af91956d9b3f677be5a603032
William Kennedy <bill@ardanlabs.com>
2019-05-25 21:58:09 +0200

    roles code
    Former-commit-id: 8a82cc2a70edfe50a62627ad24c843d891637351

---
+++ internal/platform/auth/roles.go
@@ -0,0 +1,70 @@
+package auth
+
+import (
+	"fmt"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/pkg/errors"
+)
+
+// These are the expected values for Claims.Roles.
+const (
+	RoleAdmin = "ADMIN"
+	RoleUser  = "USER"
+)
+
+// ctxKey represents the type of value for the context key.
+type ctxKey int
+
+// Key is used to store/retrieve a Claims value from a context.Context.
+const Key ctxKey = 1
+
+// Claims represents the authorization claims transmitted via a JWT.
+type Claims struct {
+	Roles []string `json:"roles"`
+	jwt.StandardClaims
+}
+
+// NewClaims constructs a Claims value for the identified user. The Claims
+// expire within a specified duration of the provided time. Additional fields
+// of the Claims can be set after calling NewClaims is desired.
+func NewClaims(subject string, roles []string, now time.Time, expires time.Duration) Claims {
+	c := Claims{
+		Roles: roles,
+		StandardClaims: jwt.StandardClaims{
+			Subject:   subject,
+			IssuedAt:  now.Unix(),
+			ExpiresAt: now.Add(expires).Unix(),
+		},
+	}
+
+	return c
+}
+
+// Valid is called during the parsing of a token.
+func (c Claims) Valid() error {
+	for _, r := range c.Roles {
+		switch r {
+		case RoleAdmin, RoleUser: // Role is valid.
+		default:
+			return fmt.Errorf("invalid role %q", r)
+		}
+	}
+	if err := c.StandardClaims.Valid(); err != nil {
+		return errors.Wrap(err, "validating standard claims")
+	}
+	return nil
+}
+
+// HasRole returns true if the claims has at least one of the provided roles.
+func (c Claims) HasRole(roles ...string) bool {
+	for _, has := range c.Roles {
+		for _, want := range roles {
+			if has == want {
+				return true
+			}
+		}
+	}
+	return false
+}
---
+++ internal/platform/conf/doc.go
@@ -0,0 +1,66 @@
+/*
+Package conf provides support for using environmental variables and command
+line arguments for configuration.
+
+It is compatible with the GNU extensions to the POSIX recommendations
+for command-line options. See
+http://www.gnu.org/software/libc/manual/html_node/Argument-Syntax.html
+
+There are no hard bindings for this package. This package takes a struct
+value and parses it for both the environment and flags. It supports several tags
+to customize the flag options.
+
+	default  - Provides the default value for the help
+	env      - Allows for overriding the default variable name.
+	flag     - Allows for overriding the default flag name.
+	short    - Denotes a shorthand option for the flag.
+	noprint  - Denotes to not include the field in any display string.
+	required - Denotes a value must be provided.
+	help     - Provides a description for the help.
+
+The field name and any parent struct name will be used for the long form of
+the command name unless the name is overridden.
+
+As an example, this config struct:
+
+	type ip struct {
+		Name string `conf:"default:localhost,env:IP_NAME_VAR"`
+		IP   string `conf:"default:127.0.0.0"`
+	}
+	type Embed struct {
+		Name     string        `conf:"default:bill"`
+		Duration time.Duration `conf:"default:1s,flag:e-dur,short:d"`
+	}
+	type config struct {
+		AnInt   int    `conf:"default:9"`
+		AString string `conf:"default:B,short:s"`
+		Bool    bool
+		Skip    string `conf:"-"`
+		IP      ip
+		Embed
+	}
+
+Would produce the following usage output:
+
+Usage: conf.test [options] [arguments]
+
+OPTIONS
+  --a-string/-s/$A_STRING         <string>    (default: B)
+  --an-int/$AN_INT                <int>       (default: 9)
+  --bool/$BOOL                    <bool>
+  --e-dur/-d/$DURATION            <duration>  (default: 1s)
+  --ip-ip/$IP_IP                  <string>    (default: 127.0.0.0)
+  --ip-name/$IP_NAME_VAR          <string>    (default: localhost)
+  --name/$NAME                    <string>    (default: bill)
+  --help/-h
+  display this help message
+
+The API is a single call to `Parse`
+
+	// Parse(args []string, namespace string, cfgStruct interface{}, sources ...Sourcer) error
+
+	if err := conf.Parse(os.Args, "CRUD", &cfg); err != nil {
+		log.Fatalf("main : Parsing Config : %v", err)
+	}
+*/
+package conf
