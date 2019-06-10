b03b123e2242fc234481e0587318cb9c1b6894c9
Jacob Walker <jacob@ardanlabs.com>
2019-06-10 12:18:43 -0500

    Combine tests.go and type_helpers.go

--- internal/platform/tests/tests.go
+++ internal/platform/tests/tests.go
@@ -112,3 +112,17 @@ func Context() context.Context {
 
 	return context.WithValue(context.Background(), web.KeyValues, &values)
 }
+
+// StringPointer is a helper to get a *string from a string. It is in the tests
+// package because we normally don't want to deal with pointers to basic types
+// but it's useful in some tests.
+func StringPointer(s string) *string {
+	return &s
+}
+
+// IntPointer is a helper to get a *int from a int. It is in the tests package
+// because we normally don't want to deal with pointers to basic types but it's
+// useful in some tests.
+func IntPointer(i int) *int {
+	return &i
+}
