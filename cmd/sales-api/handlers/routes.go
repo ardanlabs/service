218043f3a472c8cdb48cb3b39ff11825e03e80c6
William Kennedy <bill@ardanlabs.com>
2019-06-20 17:45:53 +0800

    adding the logging of Go's stack trace
    Former-commit-id: 3242cd363e18812d406acc5841aa1efa224e2ddd

--- cmd/sales-api/handlers/routes.go
+++ cmd/sales-api/handlers/routes.go
@@ -15,7 +15,7 @@ import (
 func API(shutdown chan os.Signal, log *log.Logger, db *sqlx.DB, authenticator *auth.Authenticator) http.Handler {
 
 	// Construct the web.App which holds all routes as well as common Middleware.
-	app := web.NewApp(shutdown, log, mid.Logger(log), mid.Errors(log), mid.Metrics(), mid.Panics())
+	app := web.NewApp(shutdown, log, mid.Logger(log), mid.Errors(log), mid.Metrics(), mid.Panics(log))
 
 	// Register health check endpoint. This route is not authenticated.
 	check := Check{
