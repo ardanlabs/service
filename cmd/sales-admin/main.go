8924052da24e65a47ec0615d1a4964174e243c83
William Kennedy <bill@ardanlabs.com>
2019-07-23 11:04:40 -0700

    more k8s fixes
    Former-commit-id: 382978724ed0988732585cf57fdd44fbf1f49dbd

--- cmd/sales-admin/main.go
+++ cmd/sales-admin/main.go
@@ -37,7 +37,7 @@ func run() error {
 		DB struct {
 			User       string `conf:"default:postgres"`
 			Password   string `conf:"default:postgres,noprint"`
-			Host       string `conf:"default:localhost"`
+			Host       string `conf:"default:0.0.0.0"`
 			Name       string `conf:"default:postgres"`
 			DisableTLS bool   `conf:"default:false"`
 		}
