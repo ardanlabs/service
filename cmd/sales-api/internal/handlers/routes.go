6104a0e363e3d0ecf20056065ed71f126324fbc4
William Kennedy <bill@ardanlabs.com>
2020-01-08 11:58:42 -0500

    image update commands

--- cmd/sales-api/internal/handlers/routes.go
+++ cmd/sales-api/internal/handlers/routes.go
@@ -12,14 +12,15 @@ import (
 )
 
 // API constructs an http.Handler with all application routes defined.
-func API(shutdown chan os.Signal, log *log.Logger, db *sqlx.DB, authenticator *auth.Authenticator) http.Handler {
+func API(build string, shutdown chan os.Signal, log *log.Logger, db *sqlx.DB, authenticator *auth.Authenticator) http.Handler {
 
 	// Construct the web.App which holds all routes as well as common Middleware.
 	app := web.NewApp(shutdown, mid.Logger(log), mid.Errors(log), mid.Metrics(), mid.Panics(log))
 
 	// Register health check endpoint. This route is not authenticated.
 	check := Check{
-		db: db,
+		build: build,
+		db:    db,
 	}
 	app.Handle("GET", "/v1/health", check.Health)
 
