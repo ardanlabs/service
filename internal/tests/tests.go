418c57c1c668618e0c16dab3bc57c4b0276032c6
William Kennedy <bill@ardanlabs.com>
2020-05-14 14:53:54 -0400

    naming fix

--- internal/tests/tests.go
+++ internal/tests/tests.go
@@ -122,7 +122,7 @@ func NewIntegration(t *testing.T) *Test {
 	}
 
 	// Create the logger to use.
-	logger := log.New(os.Stdout, "TEST : ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
+	log := log.New(os.Stdout, "TEST : ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
 
 	// Create RSA keys to enable authentication in our service.
 	key, err := rsa.GenerateKey(rand.Reader, 2048)
@@ -140,7 +140,7 @@ func NewIntegration(t *testing.T) *Test {
 
 	return &Test{
 		DB:            db,
-		Log:           logger,
+		Log:           log,
 		Authenticator: authenticator,
 		t:             t,
 		cleanup:       cleanup,
