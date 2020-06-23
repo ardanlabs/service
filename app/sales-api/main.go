8a5fcaf80117148535660e683d739c41a262dd30
V S Sreejeet <vsreejeet@gmail.com>
2020-06-23 23:42:56 +0530

    Removed explicit call to os.Exit(1).
    os.Exit(1) is called implicitly by log.Fatalln
    
    Or is there some specific reason for this to be called explicitly, that I am missing?

--- app/sales-api/main.go
+++ app/sales-api/main.go
@@ -43,8 +43,7 @@ func main() {
 	log := log.New(os.Stdout, "SALES : ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
 
 	if err := run(log); err != nil {
-		log.Println("main: error:", err)
-		os.Exit(1)
+		log.Fatalln("main: error:", err)
 	}
 }
 
