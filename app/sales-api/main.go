11b3ac1515123dedb3b17a646bc2afaa50945ede
William Kennedy <bill@ardanlabs.com>
2020-09-30 10:45:40 -0400

    fixing docker files for consistency

--- app/sales-api/main.go
+++ app/sales-api/main.go
@@ -65,7 +65,7 @@ func run(log *log.Logger) error {
 		}
 		Auth struct {
 			KeyID          string `conf:"default:54bb2165-71e1-41a6-af3e-7da4a0e1e2c1"`
-			PrivateKeyFile string `conf:"default:/app/private.pem"`
+			PrivateKeyFile string `conf:"default:/service/private.pem"`
 			Algorithm      string `conf:"default:RS256"`
 		}
 		Zipkin struct {
