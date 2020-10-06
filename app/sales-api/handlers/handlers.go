1cc76e068740841e8169e6ab7534b52a3f1c1259
William Kennedy <bill@ardanlabs.com>
2020-10-06 11:15:10 -0400

    fixing order of handlers

--- app/sales-api/handlers/handlers.go
+++ app/sales-api/handlers/handlers.go
@@ -35,19 +35,19 @@ func API(build string, shutdown chan os.Signal, log *log.Logger, a *auth.Auth, d
 		auth: a,
 	}
 	app.Handle(http.MethodGet, "/v1/users/:page/:rows", ug.query, mid.Authenticate(a), mid.Authorize(auth.RoleAdmin))
-	app.Handle(http.MethodPost, "/v1/users", ug.create, mid.Authenticate(a), mid.Authorize(auth.RoleAdmin))
+	app.Handle(http.MethodGet, "/v1/users/token/:kid", ug.token)
 	app.Handle(http.MethodGet, "/v1/users/:id", ug.queryByID, mid.Authenticate(a))
+	app.Handle(http.MethodPost, "/v1/users", ug.create, mid.Authenticate(a), mid.Authorize(auth.RoleAdmin))
 	app.Handle(http.MethodPut, "/v1/users/:id", ug.update, mid.Authenticate(a), mid.Authorize(auth.RoleAdmin))
 	app.Handle(http.MethodDelete, "/v1/users/:id", ug.delete, mid.Authenticate(a), mid.Authorize(auth.RoleAdmin))
-	app.Handle(http.MethodGet, "/v1/users/token/:kid", ug.token)
 
 	// Register product and sale endpoints.
 	pg := productGroup{
 		product: product.New(log, db),
 	}
 	app.Handle(http.MethodGet, "/v1/products/:page/:rows", pg.query, mid.Authenticate(a))
-	app.Handle(http.MethodPost, "/v1/products", pg.create, mid.Authenticate(a))
 	app.Handle(http.MethodGet, "/v1/products/:id", pg.queryByID, mid.Authenticate(a))
+	app.Handle(http.MethodPost, "/v1/products", pg.create, mid.Authenticate(a))
 	app.Handle(http.MethodPut, "/v1/products/:id", pg.update, mid.Authenticate(a))
 	app.Handle(http.MethodDelete, "/v1/products/:id", pg.delete, mid.Authenticate(a))
 
