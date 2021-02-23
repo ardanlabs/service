2b700cf06b8ffee79ff95f303aa38133233c65bd
William Kennedy <bill@ardanlabs.com>
2021-02-23 10:18:04 -0500

    add new code

---
+++ foundation/validate/validate.go
@@ -0,0 +1,92 @@
+// Package validate contains the support for validating models.
+package validate
+
+import (
+	"encoding/json"
+	"reflect"
+	"strings"
+
+	en "github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+	validator "gopkg.in/go-playground/validator.v9"
+	en_translations "gopkg.in/go-playground/validator.v9/translations/en"
+)
+
+// validate holds the settings and caches for validating request struct values.
+var validate *validator.Validate
+
+// translator is a cache of locale and translation information.
+var translator *ut.UniversalTranslator
+
+func init() {
+
+	// Instantiate the validator for use.
+	validate = validator.New()
+
+	// Instantiate the english locale for the validator library.
+	enLocale := en.New()
+
+	// Create a value using English as the fallback locale (first argument).
+	// Provide one or more arguments for additional supported locales.
+	translator = ut.New(enLocale, enLocale)
+
+	// Register the english error messages for validation errors.
+	lang, _ := translator.GetTranslator("en")
+	en_translations.RegisterDefaultTranslations(validate, lang)
+
+	// Use JSON tag names for errors instead of Go struct names.
+	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+		if name == "-" {
+			return ""
+		}
+		return name
+	})
+}
+
+// FieldError is used to indicate an error with a specific request field.
+type FieldError struct {
+	Field string `json:"field"`
+	Error string `json:"error"`
+}
+
+// FieldErrors represents a collection of field errors.
+type FieldErrors []FieldError
+
+// Error implments the error interface.
+func (fe FieldErrors) Error() string {
+	d, err := json.Marshal(fe)
+	if err != nil {
+		return err.Error()
+	}
+	return string(d)
+}
+
+// Check validates the provided model against it's declared tags.
+func Check(val interface{}) error {
+	if err := validate.Struct(val); err != nil {
+
+		// Use a type assertion to get the real error value.
+		verrors, ok := err.(validator.ValidationErrors)
+		if !ok {
+			return err
+		}
+
+		// lang controls the language of the error messages. You could look at the
+		// Accept-Language header if you intend to support multiple languages.
+		lang, _ := translator.GetTranslator("en")
+
+		var fields FieldErrors
+		for _, verror := range verrors {
+			field := FieldError{
+				Field: verror.Field(),
+				Error: verror.Translate(lang),
+			}
+			fields = append(fields, field)
+		}
+
+		return fields
+	}
+
+	return nil
+}
