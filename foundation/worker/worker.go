c5ae2ab1c6249562a7332c9f286455f5dc125e39
William Kennedy <bill@ardanlabs.com>
2021-04-10 18:12:11 -0400

    adding worker package for future use

---
+++ foundation/worker/worker.go
@@ -0,0 +1,122 @@
+// Package worker manages a set of registered jobs that execute on demand.
+package worker
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"github.com/google/uuid"
+)
+
+// JobFunc defines a function that can execute work for a specific job.
+type JobFunc func(ctx context.Context, payload interface{})
+
+// Worker manages jobs and the execution of those jobs concurrently.
+type Worker struct {
+	wg       sync.WaitGroup
+	mu       sync.Mutex
+	registry map[string]JobFunc
+	running  map[string]context.CancelFunc
+}
+
+// New constructs a Worker for managing and executing jobs.
+func New(registry map[string]JobFunc) *Worker {
+	return &Worker{
+		registry: registry,
+		running:  make(map[string]context.CancelFunc),
+	}
+}
+
+// Running returns the number of jobs running.
+func (w *Worker) Running() int {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	return len(w.running)
+}
+
+// Shutdown waits for all jobs to complete before it returns.
+func (w *Worker) Shutdown(ctx context.Context) error {
+
+	// Call the cancel function for all running goroutines.
+	w.mu.Lock()
+	{
+		for _, cancel := range w.running {
+			cancel()
+		}
+	}
+	w.mu.Unlock()
+
+	// Launch a goroutine to wait for all the worker goroutines
+	// to complete their work.
+	ch := make(chan struct{})
+	go func() {
+		w.wg.Wait()
+		close(ch)
+	}()
+
+	// Wait for the goroutines to report they are done or when
+	// the timeout is reached.
+	select {
+	case <-ch:
+		return nil
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}
+
+// Start lookups a job by key and launches a goroutine to perform the work. A
+// work key is returned so the caller can cancel work early.
+func (w *Worker) Start(ctx context.Context, jobKey string, payload interface{}) (string, error) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	// Locate the job in the jobs registery.
+	f, exists := w.registry[jobKey]
+	if !exists {
+		return "", fmt.Errorf("job %s is not registered", jobKey)
+	}
+
+	// Need a unique key for this work.
+	workKey := uuid.New().String()
+
+	// Create a cancel function and keep it for stop/shutdown purposes.
+	ctx, cancel := context.WithCancel(ctx)
+	w.running[workKey] = cancel
+
+	// Launch a goroutine to perform the work.
+	w.wg.Add(1)
+	go func() {
+		defer w.wg.Done()
+		defer func() { cancel(); w.removeWork(workKey) }()
+		f(ctx, payload)
+	}()
+
+	return workKey, nil
+}
+
+// Stop is used to cancel an existing job that is running.
+func (w *Worker) Stop(workKey string) error {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	// Locate the work in the running list.
+	cancel, exists := w.running[workKey]
+	if !exists {
+		return fmt.Errorf("work %s is not running", workKey)
+	}
+
+	// Call cancel to stop the work.
+	cancel()
+
+	return nil
+}
+
+// Convenience function to remove work from the running list.
+func (w *Worker) removeWork(workKey string) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	delete(w.running, workKey)
+}
---
+++ foundation/worker/worker_test.go
@@ -0,0 +1,204 @@
+package worker_test
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/ardanlabs/service/foundation/worker"
+)
+
+// Success and failure markers.
+const (
+	success = "\u2713"
+	failed  = "\u2717"
+)
+
+func TestWorker(t *testing.T) {
+	t.Log("Given the need to start work and wait for it to complete.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen handling multiple jobs", testID)
+		{
+			// Define a work function that waits to be canceled.
+			work := func(ctx context.Context, data interface{}) {
+				t := data.(*testing.T)
+				t.Logf("\t\t%s\tTest %d:\tGoroutine running.", success, testID)
+				<-ctx.Done()
+				t.Logf("\t\t%s\tTest %d:\tGoroutine terminating.", success, testID)
+			}
+
+			// Load 4 jobs in the system.
+			var jobs = map[string]worker.JobFunc{
+				"script1": work,
+				"script2": work,
+				"script3": work,
+				"script4": work,
+			}
+
+			// Create a worker and start all 4 jobs.
+			w := worker.New(jobs)
+			for key := range jobs {
+				ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+				defer cancel()
+				if _, err := w.Start(ctx, key, t); err != nil {
+					t.Fatalf("\t\t%s\tTest %d:\tShould be able to execute work: %v", failed, testID, err)
+				}
+				t.Logf("\t\t%s\tTest %d:\tShould be able to execute work.", success, testID)
+			}
+
+			// Wait for all the jobs to finish.
+			for i := 0; i < 3; i++ {
+				if w.Running() == 0 {
+					break
+				}
+				time.Sleep(100 * time.Millisecond)
+			}
+
+			// Check that the running list is empty.
+			if r := w.Running(); r != 0 {
+				t.Errorf("\t\t%s\tTest %d:\tExp: 0", failed, testID)
+				t.Errorf("\t\t%s\tTest %d:\tGot: %d", failed, testID, r)
+				t.Errorf("\t\t%s\tTest %d:\tShould be no more work running.", failed, testID)
+			} else {
+				t.Logf("\t\t%s\tTest %d:\tShould be no more work running.", success, testID)
+			}
+
+			// Shutdown the system with no work.
+			if err := w.Shutdown(context.Background()); err != nil {
+				t.Fatalf("\t\t%s\tTest %d:\tShould be able to shutdown work cleanly: %v", failed, testID, err)
+			}
+			t.Logf("\t\t%s\tTest %d:\tShould be able to shutdown work cleanly.", success, testID)
+		}
+	}
+}
+
+func TestCancelWorker(t *testing.T) {
+	t.Log("Given the need to start work and cancel it on shutdown.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen handling multiple jobs", testID)
+		{
+			// Create a WaitGroup to know when all 4 jobs have been started.
+			var wg sync.WaitGroup
+			wg.Add(4)
+
+			// Define a work function that waits to be canceled.
+			work := func(ctx context.Context, data interface{}) {
+				wg.Done()
+				t := data.(*testing.T)
+				t.Logf("\t\t%s\tTest %d:\tGoroutine running.", success, testID)
+				<-ctx.Done()
+				t.Logf("\t\t%s\tTest %d:\tGoroutine terminating.", success, testID)
+			}
+
+			// Load 4 jobs in the system.
+			var jobs = map[string]worker.JobFunc{
+				"script1": work,
+				"script2": work,
+				"script3": work,
+				"script4": work,
+			}
+
+			// Create a worker and start all 4 jobs.
+			w := worker.New(jobs)
+			for key := range jobs {
+				ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+				defer cancel()
+				if _, err := w.Start(ctx, key, t); err != nil {
+					t.Fatalf("\t%s\tTest %d:\tShould be able to execute work: %v", failed, testID, err)
+				}
+				t.Logf("\t%s\tTest %d:\tShould be able to execute work.", success, testID)
+			}
+
+			// Wait for all 4 jobs to report they are running.
+			wg.Wait()
+
+			// Give all the jobs 1 second to shutdown cleanly.
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+			defer cancel()
+			if err := w.Shutdown(ctx); err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to shutdown work cleanly: %v", failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to shutdown work cleanly.", success, testID)
+		}
+	}
+}
+
+func TestStopWorker(t *testing.T) {
+	t.Log("Given the need to start work and stop work.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen handling multiple jobs", testID)
+		{
+			// Create a WaitGroup to know when all 4 jobs have been started.
+			var wg sync.WaitGroup
+			wg.Add(4)
+
+			// Define a work function that waits to be canceled.
+			work := func(ctx context.Context, data interface{}) {
+				wg.Done()
+				t := data.(*testing.T)
+				t.Logf("\t%s\tTest %d:\tGoroutine running.", success, testID)
+				<-ctx.Done()
+				t.Logf("\t%s\tTest %d:\tGoroutine terminating.", success, testID)
+			}
+
+			// Load 4 jobs in the system.
+			var jobs = map[string]worker.JobFunc{
+				"script1": work,
+				"script2": work,
+				"script3": work,
+				"script4": work,
+			}
+
+			var works []string
+
+			// Create a worker and start all 4 jobs.
+			w := worker.New(jobs)
+			for key := range jobs {
+				ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+				defer cancel()
+				work, err := w.Start(ctx, key, t)
+				if err != nil {
+					t.Fatalf("\t%s\tTest %d:\tShould be able to execute work: %v", failed, testID, err)
+				}
+				t.Logf("\t%s\tTest %d:\tShould be able to execute work.", success, testID)
+				works = append(works, work)
+			}
+
+			// Wait for all 4 jobs to report they are running.
+			wg.Wait()
+
+			// Call Stop on all the jobs.
+			for _, work := range works {
+				w.Stop(work)
+			}
+
+			// Wait for all the jobs to finish.
+			for i := 0; i < 3; i++ {
+				if w.Running() == 0 {
+					break
+				}
+				time.Sleep(100 * time.Millisecond)
+			}
+
+			// Check that the running list is empty.
+			if r := w.Running(); r != 0 {
+				t.Errorf("\t\t%s\tTest %d:\tExp: 0", failed, testID)
+				t.Errorf("\t\t%s\tTest %d:\tGot: %d", failed, testID, r)
+				t.Errorf("\t%s\tTest %d:\tShould be no more work running.", failed, testID)
+			} else {
+				t.Logf("\t%s\tTest %d:\tShould be no more work running.", success, testID)
+			}
+
+			// Shutdown the system with no work.
+			if err := w.Shutdown(context.Background()); err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to shutdown work cleanly: %v", failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to shutdown work cleanly.", success, testID)
+
+		}
+	}
+}
