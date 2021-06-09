048c275ffc12e37a9ca35105316e50ca55b821c9
William Kennedy <bill@ardanlabs.com>
2021-06-09 14:42:42 -0400

    fix bug with calling metrics.New twice by tests

--- business/sys/metrics/metrics.go
+++ business/sys/metrics/metrics.go
@@ -1,13 +1,27 @@
 // Package metrics cosntructs the metrics the application will track.
 package metrics
 
-import "expvar"
+import (
+	"expvar"
+	"sync"
+)
 
 // ctxKeyMetric represents the type of value for the context key.
 type ctxKey int
 
 // Key is how metric values are stored/retrieved.
 const Key ctxKey = 1
+
+// =============================================================================
+
+// The expvar variables can only be initalized once. Tests will make
+// a call to New several times.
+var (
+	m  *Metrics
+	mu sync.Mutex
+)
+
+// =============================================================================
 
 // Metrics represents the set of metrics we gather. These fields are
 // safe to be accessed concurrently. No extra abstraction is required.
@@ -20,10 +34,16 @@ type Metrics struct {
 
 // New constructs the metrics that will be tracked.
 func New() *Metrics {
-	return &Metrics{
-		Goroutines: expvar.NewInt("goroutines"),
-		Requets:    expvar.NewInt("requests"),
-		Errors:     expvar.NewInt("errors"),
-		Panics:     expvar.NewInt("panics"),
+	mu.Lock()
+	defer mu.Unlock()
+
+	if m == nil {
+		m = &Metrics{
+			Goroutines: expvar.NewInt("goroutines"),
+			Requets:    expvar.NewInt("requests"),
+			Errors:     expvar.NewInt("errors"),
+			Panics:     expvar.NewInt("panics"),
+		}
 	}
+	return m
 }
