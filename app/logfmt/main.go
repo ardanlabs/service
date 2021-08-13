68938a6a4d2b1c712eb5b30c13f8dbf990b8e89d
William Kennedy <bill@ardanlabs.com>
2021-08-13 11:48:04 -0400

    reformat 0 trace id

--- app/logfmt/main.go
+++ app/logfmt/main.go
@@ -42,7 +42,7 @@ func main() {
 		}
 
 		// I like always having a traceid present in the logs.
-		traceID := "00000000000000000000000000000000"
+		traceID := "00000000-0000-0000-0000-000000000000"
 		if v, ok := m["traceid"]; ok {
 			traceID = fmt.Sprintf("%v", v)
 		}
