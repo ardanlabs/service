8b8230f09c6f97ce51451f9f62a3acba428c2afc
Jonathan Trinh <jtrinh27@pm.me>
2022-01-08 00:23:09 -0800

    Updated productgrp.go to pass product_test.go base on expected "deleteProductNotFound validates deleting a product that does not exist is not a failure."

--- app/services/sales-api/handlers/v1/productgrp/productgrp.go
+++ app/services/sales-api/handlers/v1/productgrp/productgrp.go
@@ -104,7 +104,7 @@ func (h Handlers) Delete(ctx context.Context, w http.ResponseWriter, r *http.Req
 		case errors.Is(err, product.ErrInvalidID):
 			return v1Web.NewRequestError(err, http.StatusBadRequest)
 		case errors.Is(err, product.ErrNotFound):
-			return v1Web.NewRequestError(err, http.StatusNotFound)
+			return v1Web.NewRequestError(err, http.StatusNoContent)
 		default:
 			return fmt.Errorf("querying product[%s]: %w", id, err)
 		}
