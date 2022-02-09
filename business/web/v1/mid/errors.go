b77f2787bdf0b71b88f2c57f83ab98af813333e2
William Kennedy <bill@ardanlabs.com>
2022-02-09 09:57:39 -0500

    refactor error logging

--- business/web/v1/mid/errors.go
+++ business/web/v1/mid/errors.go
@@ -32,7 +32,7 @@ func Errors(log *zap.SugaredLogger) web.Middleware {
 			if err := handler(ctx, w, r); err != nil {
 
 				// Log the error.
-				log.Errorw("ERROR", "traceid", v.TraceID, "ERROR", err)
+				log.Errorw("ERROR", "traceid", v.TraceID, "msg", err)
 
 				// Build out the error response.
 				var er v1Web.ErrorResponse
