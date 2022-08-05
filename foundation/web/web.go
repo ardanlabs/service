0abb3029ccc7685e266afcc404002ed3036d35c6
William Kennedy <bill@ardanlabs.com>
2022-08-05 10:07:49 -0400

    refactored the new code to handle checking to actually shutdown

--- foundation/web/web.go
+++ foundation/web/web.go
@@ -93,36 +93,9 @@ func (a *App) Handle(method string, group string, path string, handler Handler, 
 		// Call the wrapped handler functions.
 		if err := handler(ctx, w, r); err != nil {
 
-			// Ignore syscall.EPIPE and syscall.ECONNRESET errors which occurs
-			// when a write operation happens on the http.ResponseWriter that
-			// has simultaneously been disconnected by the client (TCP
-			// connections is broken). For instance, when large amounts of
-			// data is being written or streamed to the client.
-			// https://blog.cloudflare.com/the-complete-guide-to-golang-net-http-timeouts/
-			// https://gosamples.dev/broken-pipe/
-			// https://gosamples.dev/connection-reset-by-peer/
-			switch {
-			case errors.Is(err, syscall.EPIPE):
-
-				// Usually, you get the broken pipe error when you write to the connection after the
-				// RST (TCP RST Flag) is sent.
-				// The broken pipe is a TCP/IP error occurring when you write to a stream where the
-				// other end (the peer) has closed the underlying connection. The first write to the
-				// closed connection causes the peer to reply with an RST packet indicating that the
-				// connection should be terminated immediately. The second write to the socket that
-				// has already received the RST causes the broken pipe error.
-				return
-			case errors.Is(err, syscall.ECONNRESET):
-
-				// Usually, you get connection reset by peer error when you read from the
-				// connection after the RST (TCP RST Flag) is sent.
-				// The connection reset by peer is a TCP/IP error that occurs when the other end (peer)
-				// has unexpectedly closed the connection. It happens when you send a packet from your
-				// end, but the other end crashes and forcibly closes the connection with the RST
-				// packet instead of the TCP FIN, which is used to close a connection under normal
-				// circumstances.
-				return
-			default:
+			// Since there was an error, validate the condition of this
+			// error and determine if we need to actually shutdown or not.
+			if validateShutdown(err) {
 				a.SignalShutdown()
 				return
 			}
@@ -134,4 +107,44 @@ func (a *App) Handle(method string, group string, path string, handler Handler, 
 		finalPath = "/" + group + path
 	}
 	a.mux.Handle(method, finalPath, h)
+}
+
+// validateShutdown validates the error for special conditions that do not
+// warrent an actual shutdown by the system.
+func validateShutdown(err error) bool {
+
+	// Ignore syscall.EPIPE and syscall.ECONNRESET errors which occurs
+	// when a write operation happens on the http.ResponseWriter that
+	// has simultaneously been disconnected by the client (TCP
+	// connections is broken). For instance, when large amounts of
+	// data is being written or streamed to the client.
+	// https://blog.cloudflare.com/the-complete-guide-to-golang-net-http-timeouts/
+	// https://gosamples.dev/broken-pipe/
+	// https://gosamples.dev/connection-reset-by-peer/
+
+	switch {
+	case errors.Is(err, syscall.EPIPE):
+
+		// Usually, you get the broken pipe error when you write to the connection after the
+		// RST (TCP RST Flag) is sent.
+		// The broken pipe is a TCP/IP error occurring when you write to a stream where the
+		// other end (the peer) has closed the underlying connection. The first write to the
+		// closed connection causes the peer to reply with an RST packet indicating that the
+		// connection should be terminated immediately. The second write to the socket that
+		// has already received the RST causes the broken pipe error.
+		return false
+
+	case errors.Is(err, syscall.ECONNRESET):
+
+		// Usually, you get connection reset by peer error when you read from the
+		// connection after the RST (TCP RST Flag) is sent.
+		// The connection reset by peer is a TCP/IP error that occurs when the other end (peer)
+		// has unexpectedly closed the connection. It happens when you send a packet from your
+		// end, but the other end crashes and forcibly closes the connection with the RST
+		// packet instead of the TCP FIN, which is used to close a connection under normal
+		// circumstances.
+		return false
+	}
+
+	return true
 }
