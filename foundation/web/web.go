c4bff9c9b65f7a6186f74fbf544bb920226c265f
Jonathan Trinh <jtrinh27@pm.me>
2022-08-05 07:01:18 -0700

    handle error that occurs when the client disconnects while the http.ResponseWriter is about/being to be written to (#228)
    * handle request that has been disconnected
    
    * Adding additional comments for clarity.
    
    * fixed comment formatting.
    
    * updated comments

--- foundation/web/web.go
+++ foundation/web/web.go
@@ -3,6 +3,7 @@ package web
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"os"
 	"syscall"
@@ -91,8 +92,40 @@ func (a *App) Handle(method string, group string, path string, handler Handler, 
 
 		// Call the wrapped handler functions.
 		if err := handler(ctx, w, r); err != nil {
-			a.SignalShutdown()
-			return
+
+			// Ignore syscall.EPIPE and syscall.ECONNRESET errors which occurs
+			// when a write operation happens on the http.ResponseWriter that
+			// has simultaneously been disconnected by the client (TCP
+			// connections is broken). For instance, when large amounts of
+			// data is being written or streamed to the client.
+			// https://blog.cloudflare.com/the-complete-guide-to-golang-net-http-timeouts/
+			// https://gosamples.dev/broken-pipe/
+			// https://gosamples.dev/connection-reset-by-peer/
+			switch {
+			case errors.Is(err, syscall.EPIPE):
+
+				// Usually, you get the broken pipe error when you write to the connection after the
+				// RST (TCP RST Flag) is sent.
+				// The broken pipe is a TCP/IP error occurring when you write to a stream where the
+				// other end (the peer) has closed the underlying connection. The first write to the
+				// closed connection causes the peer to reply with an RST packet indicating that the
+				// connection should be terminated immediately. The second write to the socket that
+				// has already received the RST causes the broken pipe error.
+				return
+			case errors.Is(err, syscall.ECONNRESET):
+
+				// Usually, you get connection reset by peer error when you read from the
+				// connection after the RST (TCP RST Flag) is sent.
+				// The connection reset by peer is a TCP/IP error that occurs when the other end (peer)
+				// has unexpectedly closed the connection. It happens when you send a packet from your
+				// end, but the other end crashes and forcibly closes the connection with the RST
+				// packet instead of the TCP FIN, which is used to close a connection under normal
+				// circumstances.
+				return
+			default:
+				a.SignalShutdown()
+				return
+			}
 		}
 	}
 
