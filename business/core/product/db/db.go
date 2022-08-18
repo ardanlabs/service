94ea955b9b6d35a36991893941a7b8486f51e960
Oussama <cornos@gmail.com>
2022-08-18 15:03:26 +0200

    fix comment typo (#232)
    * fix comment typo
    
    * Update CONTRIBUTORS
    
    Add myself as a contributor

--- business/core/product/db/db.go
+++ business/core/product/db/db.go
@@ -10,7 +10,7 @@ import (
 	"go.uber.org/zap"
 )
 
-// Store manages the set of APIs for user access.
+// Store manages the set of APIs for product access.
 type Store struct {
 	log          *zap.SugaredLogger
 	tr           database.Transactor
