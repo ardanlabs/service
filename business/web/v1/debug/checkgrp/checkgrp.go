6c5594bcfc43bcc95585b2f05827b18bfe089c06
William Kennedy <bill@ardanlabs.com>
2022-11-12 00:49:31 -0500

    removed extra comments

--- business/web/v1/debug/checkgrp/checkgrp.go
+++ business/web/v1/debug/checkgrp/checkgrp.go
@@ -86,20 +86,14 @@ func (h Handlers) Liveness(w http.ResponseWriter, r *http.Request) {
 }
 
 func response(w http.ResponseWriter, statusCode int, data any) error {
-
-	// Convert the response value to JSON.
 	jsonData, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
 
-	// Set the content type and headers once we know marshaling has succeeded.
 	w.Header().Set("Content-Type", "application/json")
-
-	// Write the status code to the response.
 	w.WriteHeader(statusCode)
 
-	// Send the result back to the client.
 	if _, err := w.Write(jsonData); err != nil {
 		return err
 	}
