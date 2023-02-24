ebaa73714a537d32966a6761576e7c1e8ea28b19
William Kennedy <bill@ardanlabs.com>
2023-02-23 20:35:37 -0500

    adding custom seed function for test

--- business/data/dbmigrate/dbmigrate.go
+++ business/data/dbmigrate/dbmigrate.go
@@ -21,7 +21,7 @@ var (
 	seedDoc string
 )
 
-// Migrate attempts to bring the schema for db up to date with the migrations
+// Migrate attempts to bring the database up to date with the migrations
 // defined in this package.
 func Migrate(ctx context.Context, db *sqlx.DB) error {
 	if err := database.StatusCheck(ctx, db); err != nil {
@@ -37,9 +37,42 @@ func Migrate(ctx context.Context, db *sqlx.DB) error {
 	return d.Migrate()
 }
 
-// Seed runs the set of seed-data queries against db. The queries are ran in a
-// transaction and rolled back if any fail.
+// Seed runs the seed document defined in this package against db. The queries
+// are run in a transaction and rolled back if any fail.
 func Seed(ctx context.Context, db *sqlx.DB) (err error) {
+	if err := database.StatusCheck(ctx, db); err != nil {
+		return fmt.Errorf("status check database: %w", err)
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if errTx := tx.Rollback(); errTx != nil {
+			if errors.Is(errTx, sql.ErrTxDone) {
+				return
+			}
+			err = fmt.Errorf("rollback: %w", errTx)
+			return
+		}
+	}()
+
+	if _, err := tx.Exec(seedDoc); err != nil {
+		return fmt.Errorf("exec: %w", err)
+	}
+
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("commit: %w", err)
+	}
+
+	return nil
+}
+
+// SeedCustom runs the specified seed document against db. The queries are run
+// in a transaction and rolled back if any fail.
+func SeedCustom(ctx context.Context, db *sqlx.DB, seedDoc string) (err error) {
 	if err := database.StatusCheck(ctx, db); err != nil {
 		return fmt.Errorf("status check database: %w", err)
 	}
