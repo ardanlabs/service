37c95fcf7b66434d51b89ce36d75c19b832331fe
William Kennedy <bill@ardanlabs.com>
2023-06-10 10:58:02 -0400

    fixing caller level for sql logging

--- business/sys/database/pq/pq.go
+++ business/sys/database/pq/pq.go
@@ -154,9 +154,9 @@ func NamedExecContext(ctx context.Context, log *logger.Logger, db sqlx.ExtContex
 	q := queryString(query, data)
 
 	if _, ok := data.(struct{}); ok {
-		log.Infoc(ctx, 4, "database.NamedExecContext", "query", q)
+		log.Infoc(ctx, 5, "database.NamedExecContext", "query", q)
 	} else {
-		log.Infoc(ctx, 3, "database.NamedExecContext", "query", q)
+		log.Infoc(ctx, 4, "database.NamedExecContext", "query", q)
 	}
 
 	ctx, span := web.AddSpan(ctx, "business.sys.database.exec", attribute.String("query", q))
@@ -200,7 +200,7 @@ func NamedQuerySliceUsingIn[T any](ctx context.Context, log *logger.Logger, db s
 func namedQuerySlice[T any](ctx context.Context, log *logger.Logger, db sqlx.ExtContext, query string, data any, dest *[]T, withIn bool) error {
 	q := queryString(query, data)
 
-	log.Infoc(ctx, 4, "database.NamedQuerySlice", "query", q)
+	log.Infoc(ctx, 5, "database.NamedQuerySlice", "query", q)
 
 	ctx, span := web.AddSpan(ctx, "business.sys.database.queryslice", attribute.String("query", q))
 	defer span.End()
@@ -272,7 +272,7 @@ func NamedQueryStructUsingIn(ctx context.Context, log *logger.Logger, db sqlx.Ex
 func namedQueryStruct(ctx context.Context, log *logger.Logger, db sqlx.ExtContext, query string, data any, dest any, withIn bool) error {
 	q := queryString(query, data)
 
-	log.Infoc(ctx, 4, "database.NamedQueryStruct", "query", q)
+	log.Infoc(ctx, 5, "database.NamedQueryStruct", "query", q)
 
 	ctx, span := web.AddSpan(ctx, "business.sys.database.query", attribute.String("query", q))
 	defer span.End()
