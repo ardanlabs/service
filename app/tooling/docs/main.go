191d2f3fa945cf77cc9a5f12892c78b55ca5293f
William Kennedy <bill@ardanlabs.com>
2023-07-28 14:23:05 -0500

    finding webapi complete

--- app/tooling/docs/main.go
+++ app/tooling/docs/main.go
@@ -16,12 +16,39 @@ func main() {
 }
 
 func run() error {
+	wars, err := findWebAPIRecords()
+	if err != nil {
+		return fmt.Errorf("findWebAPIRecords, %w", err)
+	}
+
+	for _, war := range wars {
+		fmt.Printf("%#v\n", war)
+	}
+
+	return nil
+}
+
+// =============================================================================
+
+type webAPIRecord struct {
+	tag        string
+	method     string
+	route      string
+	inputType  string
+	outputType string
+	status     string
+}
+
+func findWebAPIRecords() ([]webAPIRecord, error) {
 	fset := token.NewFileSet()
 
 	file, err := parser.ParseFile(fset, "app/services/sales-api/handlers/v1/productgrp/productgrp.go", nil, parser.ParseComments)
 	if err != nil {
-		return fmt.Errorf("ParseFile: %w", err)
+		return nil, fmt.Errorf("ParseFile: %w", err)
 	}
+
+	var tag = "service:webapi"
+	var wars []webAPIRecord
 
 	ast.Inspect(file, func(n ast.Node) bool {
 		funcDecl, ok := n.(*ast.FuncDecl)
@@ -30,8 +57,19 @@ func run() error {
 			for _, cGroup := range file.Comments {
 				if fset.Position(cGroup.End()).Line == line-1 {
 					comment := cGroup.List[len(cGroup.List)-1].Text
-					if strings.Contains(comment, "service:webapi") {
-						fmt.Println(funcDecl.Name, comment[2:])
+					if n := strings.Index(comment, tag); n != -1 {
+						record := strings.Split(comment[n:], " ")
+
+						war := webAPIRecord{
+							tag:        strings.TrimSpace(record[0]),
+							method:     record[1],
+							route:      record[2],
+							inputType:  record[3],
+							outputType: record[4],
+							status:     record[5],
+						}
+
+						wars = append(wars, war)
 					}
 				}
 			}
@@ -39,5 +77,5 @@ func run() error {
 		return true
 	})
 
-	return nil
+	return wars, nil
 }
