aec0992685d1a33ee125741800f82d7eb0e7d3d0
William Kennedy <bill@ardanlabs.com>
2023-08-10 17:53:41 -0400

    fixing products endpoint doc

--- app/services/sales-api/handlers/v1/productgrp/productgrp.go
+++ app/services/sales-api/handlers/v1/productgrp/productgrp.go
@@ -152,7 +152,7 @@ func (h *Handlers) Delete(ctx context.Context, w http.ResponseWriter, r *http.Re
 }
 
 // Query returns a list of products with paging.
-// webapi method=GET route=/products outputdoc=AppProductDetail errdoc=ErrorResponse status=StatusOK
+// webapi method=GET route=/products outputdoc=AppProductDetails errdoc=ErrorResponse status=StatusOK
 func (h *Handlers) Query(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	page, err := paging.ParseRequest(r)
 	if err != nil {
