b9d74b62d30bf2da6fa083c5b24d69c44d91c163
William Kennedy <bill@ardanlabs.com>
2023-08-30 12:25:51 -0400

    missing the map over orderby fields in the store

--- business/core/home/stores/homedb/order.go
+++ business/core/home/stores/homedb/order.go
@@ -3,10 +3,15 @@ package homedb
 import (
 	"fmt"
 
+	"github.com/ardanlabs/service/business/core/home"
 	"github.com/ardanlabs/service/business/data/order"
 )
 
-var orderByFields = map[string]string{}
+var orderByFields = map[string]string{
+	home.OrderByID:     "home_id",
+	home.OrderByType:   "type",
+	home.OrderByUserID: "user_id",
+}
 
 func orderByClause(orderBy order.By) (string, error) {
 	by, exists := orderByFields[orderBy.Field]
