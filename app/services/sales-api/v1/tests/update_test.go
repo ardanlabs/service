5de4f15490ee1eb52ca02c2ec1b8bc9f1be8d947
William Kennedy <bill@ardanlabs.com>
2023-11-01 05:27:34 +0900

    added update 200 app test

--- app/services/sales-api/v1/tests/update_test.go
+++ app/services/sales-api/v1/tests/update_test.go
@@ -9,10 +9,17 @@ import (
 	"testing"
 
 	"github.com/ardanlabs/service/app/services/sales-api/v1/build/all"
+	"github.com/ardanlabs/service/app/services/sales-api/v1/handlers/homegrp"
+	"github.com/ardanlabs/service/app/services/sales-api/v1/handlers/productgrp"
+	"github.com/ardanlabs/service/app/services/sales-api/v1/handlers/usergrp"
+	"github.com/ardanlabs/service/business/core/home"
+	"github.com/ardanlabs/service/business/core/product"
 	"github.com/ardanlabs/service/business/core/user"
 	"github.com/ardanlabs/service/business/data/dbtest"
 	"github.com/ardanlabs/service/business/data/order"
 	v1 "github.com/ardanlabs/service/business/web/v1"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/uuid"
 )
 
 func updateTests(t *testing.T, tests appTest, sd seedData) {
@@ -20,7 +27,140 @@ func updateTests(t *testing.T, tests appTest, sd seedData) {
 }
 
 func testUpdate200(t *testing.T, sd seedData) []tableData {
-	return nil
+	table := []tableData{
+		{
+			name: "user",
+			url:  fmt.Sprintf("/v1/users/%s", sd.users[0].ID),
+			model: &usergrp.AppUpdateUser{
+				Name:            dbtest.StringPointer("Bill Kennedy"),
+				Email:           dbtest.StringPointer("bill@ardanlabs.com"),
+				Roles:           []string{"ADMIN"},
+				Department:      dbtest.StringPointer("IT"),
+				Password:        dbtest.StringPointer("123"),
+				PasswordConfirm: dbtest.StringPointer("123"),
+			},
+			resp: &usergrp.AppUser{},
+			expResp: &usergrp.AppUser{
+				Name:       "Bill Kennedy",
+				Email:      "bill@ardanlabs.com",
+				Roles:      []string{"ADMIN"},
+				Department: "IT",
+				Enabled:    true,
+			},
+			cmpFunc: func(x interface{}, y interface{}) string {
+				resp := x.(*usergrp.AppUser)
+				expResp := y.(*usergrp.AppUser)
+
+				if _, err := uuid.Parse(resp.ID); err != nil {
+					return "bad uuid for ID"
+				}
+
+				if resp.DateCreated == "" {
+					return "missing date created"
+				}
+
+				if resp.DateUpdated == "" {
+					return "missing date updated"
+				}
+
+				expResp.ID = resp.ID
+				expResp.DateCreated = resp.DateCreated
+				expResp.DateUpdated = resp.DateUpdated
+
+				return cmp.Diff(x, y)
+			},
+		},
+		{
+			name: "product",
+			url:  fmt.Sprintf("/v1/products/%s", sd.products[0].ID),
+			model: &productgrp.AppUpdateProduct{
+				Name:     dbtest.StringPointer("Guitar"),
+				Cost:     dbtest.FloatPointer(10.34),
+				Quantity: dbtest.IntPointer(10),
+			},
+			resp: &productgrp.AppProduct{},
+			expResp: &productgrp.AppProduct{
+				Name:     "Guitar",
+				UserID:   sd.users[0].ID.String(),
+				Cost:     10.34,
+				Quantity: 10,
+			},
+			cmpFunc: func(x interface{}, y interface{}) string {
+				resp := x.(*productgrp.AppProduct)
+				expResp := y.(*productgrp.AppProduct)
+
+				if _, err := uuid.Parse(resp.ID); err != nil {
+					return "bad uuid for ID"
+				}
+
+				if resp.DateCreated == "" {
+					return "missing date created"
+				}
+
+				if resp.DateUpdated == "" {
+					return "missing date updated"
+				}
+
+				expResp.ID = resp.ID
+				expResp.DateCreated = resp.DateCreated
+				expResp.DateUpdated = resp.DateUpdated
+
+				return cmp.Diff(x, y)
+			},
+		},
+		{
+			name: "home",
+			url:  fmt.Sprintf("/v1/homes/%s", sd.homes[0].ID),
+			model: &homegrp.AppUpdateHome{
+				Type: dbtest.StringPointer("SINGLE FAMILY"),
+				Address: &homegrp.AppUpdateAddress{
+					Address1: dbtest.StringPointer("123 Mocking Bird Lane"),
+					Address2: dbtest.StringPointer("apt 105"),
+					ZipCode:  dbtest.StringPointer("35810"),
+					City:     dbtest.StringPointer("Huntsville"),
+					State:    dbtest.StringPointer("AL"),
+					Country:  dbtest.StringPointer("US"),
+				},
+			},
+			resp: &homegrp.AppHome{},
+			expResp: &homegrp.AppHome{
+				UserID: sd.users[0].ID.String(),
+				Type:   "SINGLE FAMILY",
+				Address: homegrp.AppNewAddress{
+					Address1: "123 Mocking Bird Lane",
+					Address2: "apt 105",
+					ZipCode:  "35810",
+					City:     "Huntsville",
+					State:    "AL",
+					Country:  "US",
+				},
+			},
+			cmpFunc: func(x interface{}, y interface{}) string {
+				resp := x.(*homegrp.AppHome)
+				expResp := y.(*homegrp.AppHome)
+
+				if _, err := uuid.Parse(resp.ID); err != nil {
+					return "bad uuid for ID"
+				}
+
+				if resp.DateCreated == "" {
+					return "missing date created"
+				}
+
+				if resp.DateUpdated == "" {
+					return "missing date updated"
+				}
+
+				expResp.ID = resp.ID
+				expResp.DateCreated = resp.DateCreated
+				expResp.DateUpdated = resp.DateUpdated
+
+				return cmp.Diff(x, y)
+			},
+		},
+	}
+
+	return table
 }
 
 // =============================================================================
@@ -31,8 +171,20 @@ func updateSeed(ctx context.Context, api dbtest.CoreAPIs) (seedData, error) {
 		return seedData{}, fmt.Errorf("seeding users : %w", err)
 	}
 
+	prds, err := product.TestGenerateSeedProducts(1, api.Product, usrs[0].ID)
+	if err != nil {
+		return seedData{}, fmt.Errorf("seeding products : %w", err)
+	}
+
+	hmes, err := home.TestGenerateSeedHomes(1, api.Home, usrs[0].ID)
+	if err != nil {
+		return seedData{}, fmt.Errorf("seeding homes2 : %w", err)
+	}
+
 	sd := seedData{
-		users: usrs,
+		users:    usrs,
+		products: prds,
+		homes:    hmes,
 	}
 
 	return sd, nil
