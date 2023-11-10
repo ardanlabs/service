5776155a5d273ff6bdcabcd3a1c9988247de32de
William Kennedy <bill@ardanlabs.com>
2023-11-10 15:42:29 +1100

    added create auth tests

--- app/services/sales-api/v1/tests/create_test.go
+++ app/services/sales-api/v1/tests/create_test.go
@@ -173,7 +173,21 @@ func testCreate200(t *testing.T, app appTest, sd seedData) []tableData {
 func testCreate401(t *testing.T, app appTest, sd seedData) []tableData {
 	table := []tableData{
 		{
-			name:       "user",
+			name:       "user-emptytoken",
+			url:        "/v1/users",
+			token:      "",
+			method:     http.MethodPost,
+			statusCode: http.StatusUnauthorized,
+			resp:       &response.ErrorDocument{},
+			expResp: &response.ErrorDocument{
+				Error: "Unauthorized",
+			},
+			cmpFunc: func(x interface{}, y interface{}) string {
+				return cmp.Diff(x, y)
+			},
+		},
+		{
+			name:       "user-badtoken",
 			url:        "/v1/users",
 			token:      sd.admins[0].token[:10],
 			method:     http.MethodPost,
@@ -187,7 +201,49 @@ func testCreate401(t *testing.T, app appTest, sd seedData) []tableData {
 			},
 		},
 		{
-			name:       "product",
+			name:       "user-badsig",
+			url:        "/v1/users",
+			token:      sd.admins[0].token + "A",
+			method:     http.MethodPost,
+			statusCode: http.StatusUnauthorized,
+			resp:       &response.ErrorDocument{},
+			expResp: &response.ErrorDocument{
+				Error: "Unauthorized",
+			},
+			cmpFunc: func(x interface{}, y interface{}) string {
+				return cmp.Diff(x, y)
+			},
+		},
+		{
+			name:       "user-wronguser",
+			url:        "/v1/users",
+			token:      sd.users[0].token,
+			method:     http.MethodPost,
+			statusCode: http.StatusUnauthorized,
+			resp:       &response.ErrorDocument{},
+			expResp: &response.ErrorDocument{
+				Error: "Unauthorized",
+			},
+			cmpFunc: func(x interface{}, y interface{}) string {
+				return cmp.Diff(x, y)
+			},
+		},
+		{
+			name:       "product-emptytoken",
+			url:        "/v1/products",
+			token:      "",
+			method:     http.MethodPost,
+			statusCode: http.StatusUnauthorized,
+			resp:       &response.ErrorDocument{},
+			expResp: &response.ErrorDocument{
+				Error: "Unauthorized",
+			},
+			cmpFunc: func(x interface{}, y interface{}) string {
+				return cmp.Diff(x, y)
+			},
+		},
+		{
+			name:       "product-badtoken",
 			url:        "/v1/products",
 			token:      sd.admins[0].token[:10],
 			method:     http.MethodPost,
@@ -201,9 +257,79 @@ func testCreate401(t *testing.T, app appTest, sd seedData) []tableData {
 			},
 		},
 		{
-			name:       "home",
+			name:       "product-badsig",
+			url:        "/v1/products",
+			token:      sd.admins[0].token + "A",
+			method:     http.MethodPost,
+			statusCode: http.StatusUnauthorized,
+			resp:       &response.ErrorDocument{},
+			expResp: &response.ErrorDocument{
+				Error: "Unauthorized",
+			},
+			cmpFunc: func(x interface{}, y interface{}) string {
+				return cmp.Diff(x, y)
+			},
+		},
+		{
+			name:       "product-wronguser",
+			url:        "/v1/products",
+			token:      sd.admins[0].token,
+			method:     http.MethodPost,
+			statusCode: http.StatusUnauthorized,
+			resp:       &response.ErrorDocument{},
+			expResp: &response.ErrorDocument{
+				Error: "Unauthorized",
+			},
+			cmpFunc: func(x interface{}, y interface{}) string {
+				return cmp.Diff(x, y)
+			},
+		},
+		{
+			name:       "home-emptytoken",
+			url:        "/v1/homes",
+			token:      "",
+			method:     http.MethodPost,
+			statusCode: http.StatusUnauthorized,
+			resp:       &response.ErrorDocument{},
+			expResp: &response.ErrorDocument{
+				Error: "Unauthorized",
+			},
+			cmpFunc: func(x interface{}, y interface{}) string {
+				return cmp.Diff(x, y)
+			},
+		},
+		{
+			name:       "home-badtoken",
 			url:        "/v1/homes",
 			token:      sd.admins[0].token[:10],
+			method:     http.MethodPost,
+			statusCode: http.StatusUnauthorized,
+			resp:       &response.ErrorDocument{},
+			expResp: &response.ErrorDocument{
+				Error: "Unauthorized",
+			},
+			cmpFunc: func(x interface{}, y interface{}) string {
+				return cmp.Diff(x, y)
+			},
+		},
+		{
+			name:       "home-badsig",
+			url:        "/v1/homes",
+			token:      sd.admins[0].token + "A",
+			method:     http.MethodPost,
+			statusCode: http.StatusUnauthorized,
+			resp:       &response.ErrorDocument{},
+			expResp: &response.ErrorDocument{
+				Error: "Unauthorized",
+			},
+			cmpFunc: func(x interface{}, y interface{}) string {
+				return cmp.Diff(x, y)
+			},
+		},
+		{
+			name:       "home-wronguser",
+			url:        "/v1/homes",
+			token:      sd.admins[0].token,
 			method:     http.MethodPost,
 			statusCode: http.StatusUnauthorized,
 			resp:       &response.ErrorDocument{},
