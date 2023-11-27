b1b589b5670c31495c2904daa95516f4ccb9e188
Joseph Mukorivo <josemukorivo@gmail.com>
2023-11-27 10:42:36 +0300

    Add http shortcut methods to App

--- foundation/web/web.go
+++ foundation/web/web.go
@@ -155,6 +155,31 @@ func (a *App) Handle(method string, group string, path string, handler Handler, 
 	a.mux.Handle(method, finalPath, h)
 }
 
+// GET is a shortcut for app.Handle(http.MethodGet, path, handler).
+func (a *App) GET(group string, path string, handler Handler, mw ...Middleware) {
+	a.Handle(http.MethodGet, group, path, handler, mw...)
+}
+
+// POST is a shortcut for app.Handle(http.MethodPost, path, handler).
+func (a *App) POST(group string, path string, handler Handler, mw ...Middleware) {
+	a.Handle(http.MethodPost, group, path, handler, mw...)
+}
+
+// PUT is a shortcut for app.Handle(http.MethodPut, path, handler).
+func (a *App) PUT(group string, path string, handler Handler, mw ...Middleware) {
+	a.Handle(http.MethodPut, group, path, handler, mw...)
+}
+
+// DELETE is a shortcut for app.Handle(http.MethodDelete, path, handler).
+func (a *App) DELETE(group string, path string, handler Handler, mw ...Middleware) {
+	a.Handle(http.MethodDelete, group, path, handler, mw...)
+}
+
+// PATCH is a shortcut for app.Handle(http.MethodPatch, path, handler).
+func (a *App) PATCH(group string, path string, handler Handler, mw ...Middleware) {
+	a.Handle(http.MethodPatch, group, path, handler, mw...)
+}
+
 // =============================================================================
 
 // startSpan initializes the request by adding a span and writing otel
