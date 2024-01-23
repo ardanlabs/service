d2881ea5d897669d49b343f178685fab4ea4517c
Md.Mozaffar Hossain <Mozaffar1988@users.noreply.github.com>
2024-01-23 20:03:16 +0600

    Update request.go

--- foundation/web/request.go
+++ foundation/web/request.go
@@ -22,12 +22,14 @@ func Param(r *http.Request, key string) string {
 // body is decoded into the provided value.
 // If the provided value is a struct then it is checked for validation tags.
 // If the value implements a validate function, it is executed.
+
 func Decode(r *http.Request, val any) error {
 	decoder := json.NewDecoder(r.Body)
 	decoder.DisallowUnknownFields()
 	if err := decoder.Decode(val); err != nil {
 		return fmt.Errorf("unable to decode payload: %w", err)
 	}
+	
 
 	if v, ok := val.(validator); ok {
 		if err := v.Validate(); err != nil {
