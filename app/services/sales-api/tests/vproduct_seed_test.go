4025363be9b2d259892c0228087d5a4f5df1efb7
himynamej <javadah1376@gmail.com>
2024-03-24 11:27:45 +0330

    add user to vproduct test

--- app/services/sales-api/tests/vproduct_seed_test.go
+++ app/services/sales-api/tests/vproduct_seed_test.go
@@ -18,6 +18,32 @@ func insertVProductSeed(dbTest *dbtest.Test) (seedData, error) {
 		return seedData{}, fmt.Errorf("seeding users : %w", err)
 	}
 
+	tu1 := testUser{
+		User:  usrs[0],
+		token: dbTest.TokenV1(usrs[0].Email.Address, fmt.Sprintf("Password%s", usrs[0].Name[4:])),
+	}
+
+	// -------------------------------------------------------------------------
+
+	// -------------------------------------------------------------------------
+
+	usrs, err = user.TestGenerateSeedUsers(1, user.RoleAdmin, api.User)
+	if err != nil {
+		return seedData{}, fmt.Errorf("seeding users : %w", err)
+	}
+
+	tu2 := testUser{
+		User:  usrs[0],
+		token: dbTest.TokenV1(usrs[0].Email.Address, fmt.Sprintf("Password%s", usrs[0].Name[4:])),
+	}
+
+	// -------------------------------------------------------------------------
+
+	usrs, err = user.TestGenerateSeedUsers(1, user.RoleUser, api.User)
+	if err != nil {
+		return seedData{}, fmt.Errorf("seeding users : %w", err)
+	}
+
 	prds, err := product.TestGenerateSeedProducts(2, api.Product, usrs[0].ID)
 	if err != nil {
 		return seedData{}, fmt.Errorf("seeding products : %w", err)
@@ -50,8 +76,8 @@ func insertVProductSeed(dbTest *dbtest.Test) (seedData, error) {
 	// -------------------------------------------------------------------------
 
 	sd := seedData{
-		admins: []testUser{tu4},
-		users:  []testUser{tu3},
+		admins: []testUser{tu2, tu4},
+		users:  []testUser{tu1, tu3},
 	}
 
 	return sd, nil
