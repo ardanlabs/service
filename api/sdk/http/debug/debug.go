0f85b7ec150ba03f81de4676fcb178e8552392b1
William Kennedy <bill@ardanlabs.com>
2024-08-16 16:48:54 +0100

    added go debug information to logs

--- api/sdk/http/debug/debug.go
+++ api/sdk/http/debug/debug.go
@@ -2,10 +2,15 @@
 package debug
 
 import (
+	"context"
 	"expvar"
 	"net/http"
 	"net/http/pprof"
+	"runtime/debug"
+	"strconv"
+	"strings"
 
+	"github.com/ardanlabs/service/foundation/logger"
 	"github.com/arl/statsviz"
 )
 
@@ -26,4 +31,40 @@ func Mux() *http.ServeMux {
 	statsviz.Register(mux)
 
 	return mux
+}
+
+// LogBuildInfo logs information stored inside the Go binary.
+func LogBuildInfo(ctx context.Context, log *logger.Logger) {
+	var values []any
+
+	info, _ := debug.ReadBuildInfo()
+
+	for _, s := range info.Settings {
+		key := s.Key
+		if quoteKey(key) {
+			key = strconv.Quote(key)
+		}
+
+		value := s.Value
+		if quoteValue(value) {
+			value = strconv.Quote(value)
+		}
+
+		values = append(values, key, value)
+	}
+
+	values = append(values, "goversion", info.GoVersion)
+	values = append(values, "modversion", info.Main.Version)
+
+	log.Info(ctx, "build info", values...)
+}
+
+// quoteKey reports whether key is required to be quoted.
+func quoteKey(key string) bool {
+	return len(key) == 0 || strings.ContainsAny(key, "= \t\r\n\"`")
+}
+
+// quoteValue reports whether value is required to be quoted.
+func quoteValue(value string) bool {
+	return strings.ContainsAny(value, " \t\r\n\"`")
 }
