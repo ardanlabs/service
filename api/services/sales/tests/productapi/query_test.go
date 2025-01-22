4e6834383cc1dc66d36385fcc9cedecaa72c835e
Amir H. Meghdadian <42691663+ameghdadian@users.noreply.github.com>
2025-01-22 17:38:08 +0330

    Increase test coverage to include 400 status code scenarios for query params (#457)

--- api/services/sales/tests/productapi/query_test.go
+++ api/services/sales/tests/productapi/query_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/ardanlabs/service/app/domain/productapp"
 	"github.com/ardanlabs/service/app/sdk/apitest"
+	"github.com/ardanlabs/service/app/sdk/errs"
 	"github.com/ardanlabs/service/app/sdk/query"
 	"github.com/ardanlabs/service/business/domain/productbus"
 	"github.com/google/go-cmp/cmp"
@@ -35,6 +36,37 @@ func query200(sd apitest.SeedData) []apitest.Table {
 				Total:       len(prds),
 				Items:       toAppProducts(prds),
 			},
+			CmpFunc: func(got any, exp any) string {
+				return cmp.Diff(got, exp)
+			},
+		},
+	}
+
+	return table
+}
+
+func query400(sd apitest.SeedData) []apitest.Table {
+	table := []apitest.Table{
+		{
+			Name:       "bad-query-filter",
+			URL:        "/v1/products?page=1&rows=10&name=$#!",
+			Token:      sd.Admins[0].Token,
+			StatusCode: http.StatusBadRequest,
+			Method:     http.MethodGet,
+			GotResp:    &errs.Error{},
+			ExpResp:    errs.Newf(errs.InvalidArgument, "[{\"field\":\"name\",\"error\":\"invalid name \\\"$#!\\\"\"}]"),
+			CmpFunc: func(got any, exp any) string {
+				return cmp.Diff(got, exp)
+			},
+		},
+		{
+			Name:       "bad-orderby-value",
+			URL:        "/v1/products?page=1&rows=10&orderBy=roduct_id,ASC",
+			Token:      sd.Admins[0].Token,
+			StatusCode: http.StatusBadRequest,
+			Method:     http.MethodGet,
+			GotResp:    &errs.Error{},
+			ExpResp:    errs.Newf(errs.InvalidArgument, "[{\"field\":\"order\",\"error\":\"unknown order: roduct_id\"}]"),
 			CmpFunc: func(got any, exp any) string {
 				return cmp.Diff(got, exp)
 			},
