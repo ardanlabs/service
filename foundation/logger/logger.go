337023778e8473f195336bdbd01abffbf45b1d13
ameghdadian <amirthedevops@gmail.com>
2025-06-12 14:27:36 +0330

    Refactor logger Discard functionality

--- foundation/logger/logger.go
+++ foundation/logger/logger.go
@@ -46,78 +46,50 @@ func NewStdLogger(logger *Logger, level Level) *log.Logger {
 
 // Debug logs at LevelDebug with the given context.
 func (log *Logger) Debug(ctx context.Context, msg string, args ...any) {
-	if log.discard {
-		return
-	}
-
 	log.write(ctx, LevelDebug, 3, msg, args...)
 }
 
 // Debugc logs the information at the specified call stack position.
 func (log *Logger) Debugc(ctx context.Context, caller int, msg string, args ...any) {
-	if log.discard {
-		return
-	}
-
 	log.write(ctx, LevelDebug, caller, msg, args...)
 }
 
 // Info logs at LevelInfo with the given context.
 func (log *Logger) Info(ctx context.Context, msg string, args ...any) {
-	if log.discard {
-		return
-	}
-
 	log.write(ctx, LevelInfo, 3, msg, args...)
 }
 
 // Infoc logs the information at the specified call stack position.
 func (log *Logger) Infoc(ctx context.Context, caller int, msg string, args ...any) {
-	if log.discard {
-		return
-	}
-
 	log.write(ctx, LevelInfo, caller, msg, args...)
 }
 
 // Warn logs at LevelWarn with the given context.
 func (log *Logger) Warn(ctx context.Context, msg string, args ...any) {
-	if log.discard {
-		return
-	}
-
 	log.write(ctx, LevelWarn, 3, msg, args...)
 }
 
 // Warnc logs the information at the specified call stack position.
 func (log *Logger) Warnc(ctx context.Context, caller int, msg string, args ...any) {
-	if log.discard {
-		return
-	}
-
 	log.write(ctx, LevelWarn, caller, msg, args...)
 }
 
 // Error logs at LevelError with the given context.
 func (log *Logger) Error(ctx context.Context, msg string, args ...any) {
-	if log.discard {
-		return
-	}
-
 	log.write(ctx, LevelError, 3, msg, args...)
 }
 
 // Errorc logs the information at the specified call stack position.
 func (log *Logger) Errorc(ctx context.Context, caller int, msg string, args ...any) {
-	if log.discard {
-		return
-	}
-
 	log.write(ctx, LevelError, caller, msg, args...)
 }
 
 func (log *Logger) write(ctx context.Context, level Level, caller int, msg string, args ...any) {
 	slogLevel := slog.Level(level)
+
+	if log.discard {
+		return
+	}
 
 	if !log.handler.Enabled(ctx, slogLevel) {
 		return
