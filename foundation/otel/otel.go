6c39f24bd7747e39697450c481739333db0eadb7
Florin Pățan <florinpatan@gmail.com>
2025-12-12 19:11:57 +0200

    Small cleanup (#621)
    This commit removes the depdendency between otel and log
    foundation packages.
    
    It also removes the usage of defer in for loops, which in certain
    cases can create resource leaks thus it's better not to use that
    construct, if possible.

--- foundation/otel/otel.go
+++ foundation/otel/otel.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/ardanlabs/service/foundation/logger"
 	"github.com/google/uuid"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
@@ -32,7 +31,7 @@ type Config struct {
 }
 
 // InitTracing configures open telemetry to be used with the service.
-func InitTracing(log *logger.Logger, cfg Config) (trace.TracerProvider, func(ctx context.Context), error) {
+func InitTracing(cfg Config) (trace.TracerProvider, func(ctx context.Context), error) {
 
 	// WARNING: The current settings are using defaults which may not be
 	// compatible with your project. Please review the documentation for
@@ -54,12 +53,9 @@ func InitTracing(log *logger.Logger, cfg Config) (trace.TracerProvider, func(ctx
 
 	switch cfg.Host {
 	case "":
-		log.Info(context.Background(), "OTEL", "tracer", "NOOP")
 		traceProvider = noop.NewTracerProvider()
 
 	default:
-		log.Info(context.Background(), "OTEL", "tracer", cfg.Host)
-
 		tp := sdktrace.NewTracerProvider(
 			sdktrace.WithSampler(sdktrace.ParentBased(newEndpointExcluder(cfg.ExcludedRoutes, cfg.Probability))),
 			sdktrace.WithBatcher(exporter,
